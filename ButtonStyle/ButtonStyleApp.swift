109e7ccb627cc90b2c0fcd66e1e66459fd94da70
Will Ellis <wiellis@gmail.com>
2023-02-02 10:24:54 -0500

    Initial Commit

---
+++ ButtonStyle/ButtonStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonStyleApp.swift
+//  ButtonStyle
+//
+//  Created by Will Ellis on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ButtonStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
